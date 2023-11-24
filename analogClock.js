0d04aece0dad4a68513d0dd65c03ca76f65538c6
Cyril Tobler <cyriltobler@icloud.com>
2023-11-24 14:39:38 +0100

    added JS comments

--- analogClock.js
+++ analogClock.js
@@ -4,7 +4,7 @@ function draw(){
     const minutes = date.getMinutes()
     const seconds = date.getSeconds()
 
-
+    //rotate the hands
     const secondsRotation = seconds / 60 * 360
     document.querySelector('.second-hand').style.transform = `rotate(${secondsRotation}deg)`
     const minutesRotation = minutes / 60 * 360 + secondsRotation / 360
@@ -12,16 +12,20 @@ function draw(){
     const hourRotation = hours / 12 * 360 + minutesRotation / 360
     document.querySelector('.hour-hand').style.transform = `rotate(${hourRotation}deg)`
 
+    //activate day/night modus
     checkTime(hours)
+
+    //repeat this function
     requestAnimationFrame(draw)
 }
 
-//set the clock
+//write the numbers in the marking
 document.querySelector('.marking-one').innerHTML = "12"
 document.querySelector('.marking-two').innerHTML = "3"
 document.querySelector('.marking-three').innerHTML = "6"
 document.querySelector('.marking-four').innerHTML = "9"
 
+//day/nigth switch function
 function checkTime(hours){
     if(hours > 18 || hours < 6){
         //Design for the night
