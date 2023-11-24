50e1d1bfae6ae268078f507190743d0c1daae702
Cyril Tobler <cyriltobler@icloud.com>
2023-11-24 09:15:33 +0100

    added JS animation

--- analogClock.js
+++ analogClock.js
@@ -1,8 +1,18 @@
+function draw(){
+    const date = new Date()
+    const hours = date.getHours() + 1
+    const minutes = date.getMinutes()
+    const seconds = date.getSeconds()
 
 
-const date = new Date()
-const hours = date.getHours()
-const minutes = date.getMinutes()
-const seconds = date.getSeconds()
+    const secondsRotation = seconds / 60 * 360
+    document.querySelector('.second-hand').style.transform = `rotate(${secondsRotation}deg)`
+    const minutesRotation = minutes / 60 * 360
+    document.querySelector('.min-hand').style.transform = `rotate(${minutesRotation}deg)`
+    const hourRotation = hours / 24 * 360
+    document.querySelector('.hour-hand').style.transform = `rotate(${hourRotation}deg)`
 
+    requestAnimationFrame(draw)
+}
 
+draw()
