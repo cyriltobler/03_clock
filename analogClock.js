77fbeaf8bac4c7424e1baac2fec815c7cc2c89e9
Cyril Tobler <cyriltobler@icloud.com>
2023-11-24 14:34:28 +0100

    day/night design

--- analogClock.js
+++ analogClock.js
@@ -1,6 +1,6 @@
 function draw(){
     const date = new Date()
-    const hours = date.getHours()
+    const hours = date.getHours() + 8
     const minutes = date.getMinutes()
     const seconds = date.getSeconds()
 
@@ -12,14 +12,36 @@ function draw(){
     const hourRotation = hours / 12 * 360 + minutesRotation / 360
     document.querySelector('.hour-hand').style.transform = `rotate(${hourRotation}deg)`
 
+    checkTime(hours)
     requestAnimationFrame(draw)
 }
 
-
+//set the clock
 document.querySelector('.marking-one').innerHTML = "12"
 document.querySelector('.marking-two').innerHTML = "3"
 document.querySelector('.marking-three').innerHTML = "6"
 document.querySelector('.marking-four').innerHTML = "9"
 
+function checkTime(hours){
+    if(hours > 18 || hours < 6){
+        //Design for the night
+        document.querySelector('body').style.backgroundColor = "#222"
+        document.querySelectorAll('.marking').forEach(element =>{
+        element.style.color = "wheat"
+    })
+    document.querySelectorAll('.hand').forEach(element =>{
+        element.style.backgroundColor = "wheat"
+    })
+    }else{
+        //Design for the day
+        document.querySelector('body').style.backgroundColor = "#fff"
+        document.querySelectorAll('.marking').forEach(element =>{
+            element.style.color = "black"
+        })
+        document.querySelectorAll('.hand').forEach(element =>{
+            element.style.backgroundColor = "black"
+        })
+    }
+}
 
 draw()
