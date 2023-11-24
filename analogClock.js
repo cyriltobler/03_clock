d70d7ff72d1b42f6e1b389a6ca5dc8a1253a7091
Cyril Tobler <cyriltobler@icloud.com>
2023-11-24 09:24:24 +0100

    added rotation calculation in js

--- analogClock.js
+++ analogClock.js
@@ -1,15 +1,15 @@
 function draw(){
     const date = new Date()
-    const hours = date.getHours() + 1
+    const hours = date.getHours()
     const minutes = date.getMinutes()
     const seconds = date.getSeconds()
 
 
     const secondsRotation = seconds / 60 * 360
     document.querySelector('.second-hand').style.transform = `rotate(${secondsRotation}deg)`
-    const minutesRotation = minutes / 60 * 360
+    const minutesRotation = minutes / 60 * 360 + seconds / 60
     document.querySelector('.min-hand').style.transform = `rotate(${minutesRotation}deg)`
-    const hourRotation = hours / 24 * 360
+    const hourRotation = hours / 12 * 360 + minutesRotation / 360
     document.querySelector('.hour-hand').style.transform = `rotate(${hourRotation}deg)`
 
     requestAnimationFrame(draw)
